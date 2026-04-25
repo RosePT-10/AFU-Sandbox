1882e45a1a1ad773f795e7a5ba2b781fb160625d
Tal-01 <rose.petal.thorn@tutamail.com>
2026-04-24 20:28:56 -0600

    Created project file

---
+++ Core.cs
@@ -0,0 +1,18 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(Sandbox.Core), "Sandbox", "1.0.0", "taldo", null)]
+[assembly: MelonGame("Videocult", "Airframe")]
+
+namespace Sandbox
+{
+    public class Core : MelonMod
+    {
+        public UnityEngine.Vector3 camera_aim_point;
+
+        
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
